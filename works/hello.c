842ffcbdde3b874d628b997fb27bfc6b5cb6a415
user <Tatjana.Solovjova@rtu.lv>
2019-09-30 11:55:46 +0300

    20190930_11_55

---
+++ works/hello.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+// this library should be added for printf to work
+
+void main()
+ {
+// Hello World!; // absolutely wrong!
+// "Hello World!"; // working, but text doesn't appear on screen
+ printf("Hello World!\n");
+ }

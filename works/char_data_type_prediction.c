b7b56bd7314afe60e1ea71823204443be8c96184
user <Tatjana.Solovjova@rtu.lv>
2019-10-28 14:09:27 +0200

    20191028_14_10

---
+++ works/char_data_type_prediction.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main()
+ {
+ char b=127;
+ printf("b char variable value as decimal namber (?127): %d\n",b);
+
+ char c=128;
+ printf("c char variable value as decimal namber (?128): %d\n",c);
+
+ char d=129;
+ printf("d char variable value as decimal namber (?129): %d\n",d);
+ }
